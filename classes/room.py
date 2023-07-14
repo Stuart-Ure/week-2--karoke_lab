8774919d1d2e82aa057a229939114d354bfbcd3a
Stuart <stuartrure@gmail.com>
2023-07-14 08:42:34 +0100

    mvp completed

--- classes/room.py
+++ classes/room.py
@@ -10,8 +10,13 @@ class Room:
     def add_guest_to_room(self, guest):
         self.guests.append(guest)
 
+    # def remove_guest_from_room(self, guest):
+    #     self.guests.remove(guest)
+
     def remove_guest_from_room(self, guest):
-        self.guests.remove(guest)
+        if guest in self.guests:
+            self.guests.remove(guest)
+
 
     def add_song_to_room(self, song):
         self.songs.append(song)
