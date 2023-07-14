fad7d311248c6a0e019a30d1c3ddca96958eefac
Stuart <stuartrure@gmail.com>
2023-07-14 09:27:35 +0100

    updated code

--- classes/room.py
+++ classes/room.py
@@ -10,12 +10,12 @@ class Room:
     def add_guest_to_room(self, guest):
         self.guests.append(guest)
 
-    # def remove_guest_from_room(self, guest):
-    #     self.guests.remove(guest)
-
     def remove_guest_from_room(self, guest):
-        if guest in self.guests:
-            self.guests.remove(guest)
+        self.guests.remove(guest)
+
+    # def remove_guest_from_room(self, guest):
+    #     if guest in self.guests:
+    #         self.guests.remove(guest)
 
 
     def add_song_to_room(self, song):
--- tests/room_test.py
+++ tests/room_test.py
@@ -11,12 +11,20 @@ class TestRoom(unittest.TestCase):
         self.song2 = Song ("All my life", " Foo Fighters")
         self.song3 = Song ("Blank Space", "Taylor Swift")
 
+        self.songs = [self.song1, self.song2, self.song3]
+    
         self.room1 = Room ("NYC Room", 3)  #need to add person to the room in the setup before the remove function can work, need to add attendance.
         self.room2 = Room ("Leith room", 5) 
+
+        self.rooms = [self.room1, self.room2]
 
         self.guest1 = Guest ("Alfredo")
         self.guest2 = Guest ("Winston")
         self.guest3 = Guest ("Jesus")
+
+        self.guests= [self.guest1, self.guest2, self.guest3]
+
+        self.guest4= Guest ("Brad")
     
 
     def test_room_has_name(self):
@@ -24,16 +32,22 @@ class TestRoom(unittest.TestCase):
 
     def test_room_capacity(self):
         self.assertEqual (5, self.room2.capacity)
+        self.assertEqual ( 3 ,self.room1.capacity)
 
 
     def test_add_guest_to_room(self):
-        self.room2.add_guest_to_room(self.guest2)
+        self.room2.add_guest_to_room(self.guest1)
         self.assertEqual(1, len(self.room2.guests))
 
+
     def test_remove_guest_from_room(self):
+        self.room2.add_guest_to_room(self.guest2)
         self.room2.remove_guest_from_room(self.guest2)
         self.assertEqual(0,len(self.room2.guests))
 
     def test_add_song_to_room(self):
         self.room1.add_song_to_room(self.song1)
         self.assertEqual(1, len(self.room1.songs))
+
+
+    
